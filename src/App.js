f8db99646ba13f1c9867078614d43f247a324969
canweb3 <canoguzorhan066@gmail.com>
2022-11-28 19:04:26 +0300

    react-scanner2

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useRef, useState } from 'react';
+
+function App() {
+
+  const video = useRef(null);
+  const canvas = useRef(null);
+  const [barcode, setBarcode] = useState(null);
+
+  const openCam = () => {
+    navigator.mediaDevices.getUserMedia({ 
+      video: { 
+        width: 300, 
+        height: 300, 
+        facingMode: 'environment' // mobilde arka kamerayı açtırmak için
+      }
+    })
+    .then(stream => {
+      video.current.srcObject = stream;
+      video.current.play();
+
+      const ctx = canvas.current.getContext('2d');
+      const barcode = new window.BarcodeDetector({formats: ['qr_code', 'ean_13']});
+      setInterval(() => {
+        canvas.current.width = video.current.videoWidth;
+        canvas.current.height = video.current.videoHeight;
+        ctx.drawImage(video.current, 0, 0, video.current.videoWidth, video.current.videoHeight);
+        barcode.detect(canvas.current)
+          .then(([data]) => {
+            if (data) {
+              setBarcode(data.rawValue);
+            }
+          })
+          .catch(err => console.log(err))
+      }, 100)
+
+    })
+    .catch(err => alert(err))
+  }
+
+  return (
+    <>
+      <button onClick={openCam}>Kamerayı Aç</button>
+      <div>
+        <video ref={video} autoPlay muted hidden />
+        <canvas ref={canvas} />
+      </div>
+      {barcode && (
+        <div>
+          Bulunan barkod: {barcode}
+        </div>
+      )}
+      
+    </>
+  );
+}
+
+export default App;
